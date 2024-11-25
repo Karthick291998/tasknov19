819511c5125a50a2bc106b655a068118495f6dbc
Karthick Subramanian <165255079+Karthick291998@users.noreply.github.com>
2024-11-25 12:25:47 +0530

    Update testRunnerPharmEasy.java
    commit the plugin target,reprt

--- nov19/src/test/java/testRunner/testRunnerPharmEasy.java
+++ nov19/src/test/java/testRunner/testRunnerPharmEasy.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\Users\\karth\\eclipse-workspace\\nov19\\src\\test\\resources\\TaskNov19\\featureFiles",
-glue={"TaskNov19","HOOKS"},dryRun=false,monochrome=true)
+glue={"TaskNov19","HOOKS"},dryRun=false,monochrome=true,plugin= {"html:target/html","json:target/report.json"})
 public class testRunnerPharmEasy {
 
 }
